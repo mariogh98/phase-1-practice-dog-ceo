be377ade4620f5459367c21c80dfc8e5043ec202
Mario Gentry-Humphrey <mario.gentry89@yahoo.com>
2022-06-16 23:27:59 -0500

    commit tree 76bbb4792754b9481168e329b394d117d01b110e

--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
-console.log('%c HI', 'color: firebrick')
+
+
+function getBreeds() {
+     const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+     return fetch(imgUrl)
+    .then(response => response.json())
+    .then(response => {
+        const dogImageContainer = document.getElementById("dog-image-container")
+        response.message.forEach(url => {
+            const img = document.createElement("img")
+            img.src = url
+            dogImageContainer.append(img)
+        })
+    })
+}
+
+let breeds = []
+
+function getBreedNames() {
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl)
+    .then(response => response.json())
+    .then(response => {
+        breeds = Object.keys(response.message)
+        addBreedNames(breeds)
+    })
+}
+
+function addBreedNames(breeds) {
+    const ul = document.querySelector("#dog-breeds")
+    breeds.map(breed => {
+        const li = document.createElement("li")
+        li.textContent = breed
+        ul.append(li)
+    })
+}
+
+document.addEventListener("click", event => {
+    if(event.target.matches("li")){
+        event.target.style.color = "blue"
+    }
+})
+
+
+document.addEventListener("change", event =>  {
+    if(event.target.matches("#breed-dropdown")){
+    const ul = document.querySelector("#dog-breeds")
+        ul.innerHTML = ""
+        const filterBreeds = breeds.filter(breed => breed[0] === event.target.value)
+        addBreedNames(filterBreeds)
+    }
+        
+})
+    
+
+getBreedNames()
+getBreeds()
